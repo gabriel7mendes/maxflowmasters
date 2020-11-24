28965459b241f318bf557dc26792ac938b38d2e6
gabriel7mendes <andrewsseven@gmail.com>
2020-11-24 17:46:56 -0300

    modifying algorithm structure

--- src/main/java/br/com/maxflow/algorithm/app/EdmondsKarpCase1App.java
+++ src/main/java/br/com/maxflow/algorithm/app/EdmondsKarpCase1App.java
@@ -2,6 +2,8 @@ package br.com.maxflow.algorithm.app;
 
 import java.io.IOException;
 
+import br.com.maxflow.algorithm.EdmondsKarp;
+
 public class EdmondsKarpCase1App {
 
 	public static void main(String[] args) throws IOException {
@@ -16,6 +18,25 @@ public class EdmondsKarpCase1App {
 		 * 
 		 * scanner.close();
 		 */
+		
+		int m = 10;
+		EdmondsKarp edmondsKarp = new EdmondsKarp(m-1);
+		
+		int s = 0;
+		int t = 5;
+		
+		edmondsKarp.addEdge(s, 1, 16);
+		edmondsKarp.addEdge(s, 2, 13);
+		edmondsKarp.addEdge(1, 2, 10);
+		edmondsKarp.addEdge(2, 1, 4);
+		edmondsKarp.addEdge(1, 3, 12);
+		edmondsKarp.addEdge(2, 4, 14);
+		edmondsKarp.addEdge(3, 2, 9);
+		edmondsKarp.addEdge(3, t, 20);
+		edmondsKarp.addEdge(4, 3, 7);
+		edmondsKarp.addEdge(4, t, 4);
+		
+		System.out.println(edmondsKarp.maxFlow(s, t));
 	}
 	
 }
