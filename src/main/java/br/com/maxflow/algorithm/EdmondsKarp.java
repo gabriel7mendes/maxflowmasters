6043202b352848dbca749f194eb6bf7e5ebb655f
gabriel7mendes <andrewsseven@gmail.com>
2020-11-24 17:12:39 -0300

    modifying algorithm structure

--- src/main/java/br/com/maxflow/algorithm/EdmondsKarp.java
+++ src/main/java/br/com/maxflow/algorithm/EdmondsKarp.java
@@ -2,15 +2,21 @@ package br.com.maxflow.algorithm;
 
 import br.com.datastructures.queue.Queue;
 import br.com.maxflow.graph.Edge;
+import br.com.maxflow.graph.Graph;
 import br.com.maxflow.graph.Node;
 
-public class EdmondsKarp extends FordFulkerson {
+public class EdmondsKarp {
+	
+	private Graph graph;
 
 	public EdmondsKarp(int n) {
-		super(n);
+		graph = new Graph(n);  
 	}
-
-	@Override
+	 	
+	public void addEdge(int from, int to, int cap) {
+		graph.addEdge(from, to, cap);
+	}
+	
 	protected boolean hasPath(int source, int sink) {
 		Queue<Node> queue = new Queue<>();
 	    
@@ -37,4 +43,29 @@ public class EdmondsKarp extends FordFulkerson {
 		
 		return true;
 	}
+	
+	public int maxFlow(int source, int sink) {
+		int maxFlow = 0;
+		
+		while(true)
+		{
+			graph.resetParent();
+			
+			if (!hasPath(source,sink)) 
+                break;
+			
+        	int df = Integer.MAX_VALUE;
+        	
+        	for(Edge edge = graph.getParent(sink); edge != null; edge = graph.getParent(edge.getFrom()))
+       		    df = Math.min(df, edge.getCapacity() - edge.getFlow());
+       	
+	       	for(Edge edge = graph.getParent(sink); edge != null; edge = graph.getParent(edge.getFrom()))
+	       		edge.pushFlow(df);	 
+         
+        	maxFlow += df;
+		}
+		
+		return maxFlow;
+	}
+
 }
