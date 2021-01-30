b60d4987c700c845f54442d318eee04736192a1e
gabriel7mendes <andrewsseven@gmail.com>
2021-01-30 09:54:38 -0300

    refactoring ford-fulkerson method

--- src/main/java/br/com/maxflow/app/FordFulkersonEmaApp1.java
+++ src/main/java/br/com/maxflow/app/FordFulkersonEmaApp1.java
@@ -1,5 +1,6 @@
 package br.com.maxflow.app;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -10,6 +11,7 @@ public class FordFulkersonEmaApp1 {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		
+		String fileName = args[0];
 		int m = sc.nextInt();
 		int cap = 1000;
 		
@@ -21,6 +23,11 @@ public class FordFulkersonEmaApp1 {
 		fordFulkersonEma.createEdges(cap, m, s, t);
 		
 		System.out.println("maxflow: " + fordFulkersonEma.maxFlow(s, t));
+		System.out.println("number of steps: " + fordFulkersonEma.steps());
+		
+		FileWriter fw = new FileWriter(fileName);
+		fw.write(fordFulkersonEma.steps() + "\n");
+		fw.close();
 		
 		sc.close();
 	}
